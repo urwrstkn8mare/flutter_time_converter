bb70a9392a904bcdb978f499164cec83d3641161
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2020-01-25 20:02:53 +0800

    just worked on search page a little

--- lib/src/search/searchPage.dart
+++ lib/src/search/searchPage.dart
@@ -3,8 +3,22 @@ import 'package:flutter/material.dart';
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Search"),
+    return Material(
+      child: Container(
+        child: Container(
+          margin: EdgeInsets.only(top: 30.0, left: 30.0, right: 30.0),
+          color: Colors.red,
+          child: Text(
+            "Search",
+            style: TextStyle(
+              fontWeight: FontWeight.w900 /* black */,
+              fontSize: 50.0,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        color: Colors.white,
+      ),
     );
   }
 }
